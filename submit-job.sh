66880fbd2ba7fb8fd28df7f6d1f0c720c34252e2
dhruvdh <dhruv.dhamani@gmail.com>
2025-04-19 23:09:21 -0400

    Update SLURM config with 4 GPUs, 48hr runtime and CUDA 12.4 support

--- submit-job.sh
+++ submit-job.sh
@@ -2,5 +2,5 @@
 
 source cluster-setup.sh
 cd "$SLURM_SUBMIT_DIR"
-uv run main.py --generate-slurm
+uv run main.py --generate-slurm --generate-slurm --slurm-gpus 4 --slurm-time 48:00:00
 sbatch slurm_jobs/experiments_array.slurm
